0dce3eb001a1cdf9be883e65c143c10d681a1f49
ibrahimkertmen <kertmen2813@hotmail.com>
2020-12-26 00:41:02 +0300

    countertekrar finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+
+  // azalt = () => {
+  //   this.setState({
+  //     count: this.state.count - 1
+  //   })
+  // }
+
+  // artır = () => {
+  //   this.setState({
+  //     count: this.state.count + 1
+  //   })
+  // }
+
+  // yukarıdaki ikisni "degistir" olarak tek fonksiyonda yazdık o yüzden onlara gerek kalmadı.
+
+  degistir = (operation) => {
+    this.setState({
+      count: operation === "azalt" ? this.state.count - 1 : this.state.count + 1
+    })
+  }
+
+  render(){
+    const {count} = this.state;
+    return <div>
+        <h1>{count}</h1>
+        <button onClick={() => {this.degistir("azalt")}}>
+          -
+          </button>
+        <button onClick={() => {this.degistir("artır")}}>
+          +
+          </button>
+      </div>
+  }
 }
+
+
+
+
 
 export default App;
